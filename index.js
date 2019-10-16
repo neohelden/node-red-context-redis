896b8924c2534432958c5549e2e275edf6beac7a
Matteo Piccinini <matteo.piccinini@gruppofilippetti.it>
2019-10-16 16:48:47 +0200

    NodeRed util update, Implemented retry strategy

--- index.js
+++ index.js
@@ -175,8 +175,29 @@ function Redis(config) {
         db: config.db || 0,
         password: config.password,
         tls: config.tls,
-        //Do not try to reconnect, throw an error
-        retry_strategy: () => undefined
+        retry_strategy: (options) => {
+            if (options.error && options.error.code === 'ENOTFOUND') {
+                // End reconnecting on a specific error and flush all commands with
+                // a individual error
+                return new Error('The server cannot be found');
+            }            
+            if (options.error && options.error.code === 'ECONNREFUSED') {
+                // End reconnecting on a specific error and flush all commands with
+                // a individual error
+                return new Error('The server refused the connection');
+            }
+            if (options.total_retry_time > 1000 * 60 * 60) {
+                // End reconnecting after a specific timeout and flush all commands
+                // with a individual error
+                return new Error('Retry time exhausted');
+            }
+            if (options.attempt > 10) {
+                // End reconnecting with built in error
+                return undefined;
+            }
+            // reconnect after
+            return Math.min(options.attempt * 100, 3000);
+        }
     };
     this.client = null;
     this.knownCircularRefs = {};
